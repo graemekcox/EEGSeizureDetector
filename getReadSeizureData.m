82783c871fc9d4428c2ee316873b9203a4a4ce5b
Graeme <graeme.k.cox@gmail.com>
2018-02-20 12:49:57 -0500

    - replaced string labels with integer values\n- added basic SVM functionality

--- getReadSeizureData.m
+++ getReadSeizureData.m
@@ -2,27 +2,44 @@ function [labels, features] = getReadSeizureData(root)
 
     %Get all interIcalClips in the folder
     interIctalClips = dir([root '*_interictal_*.mat']);
+    ictalClips = dir([root '*_ictal_*.mat']);
     
     %Number of features being used
     numFeat = 5;
     features = zeros(1,numFeat);
-    labels = {};
+%     labels = {};
+    labels = zeros(1,1);
     
     disp('Reading in Interictal data');
     
-    
-    index = 1;
+    % Read in interictal clips
     for i=1:size(interIctalClips,1)
 
         file = [root interIctalClips(i).name];
         
         %Function takes in label and file name
-        [label,feat] = waveletFeatureExtractor(file, 'interictal');
-        
+%         [label,feat] = waveletFeatureExtractor(file, 'interictal');
+        [label,feat] = waveletFeatureExtractor(file, 1);
         %%Add features and labels to arrays
         features(end+1:end+size(feat,1), 1:numFeat) = feat;
         labels = [labels; label'];
     end
+    labels(1) = [];
     features(1,:) = []; %First row is filled with zeros
     
+   
+    %% Read in ictal clips
+    disp('Reading in Ictal data')
+    for i=1:size(ictalClips,1)
+
+        file = [root ictalClips(i).name];
+        
+      %  Function takes in label and file name
+%         [label,feat] = waveletFeatureExtractor(file, 'interictal');
+        [label,feat] = waveletFeatureExtractor(file, -1);
+        %Add features and labels to arrays
+        features(end+1:end+size(feat,1), 1:numFeat) = feat;
+        labels = [labels; label'];
+    end
+
 end
