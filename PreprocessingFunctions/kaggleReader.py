f621ab36fd0204ca14843cff4b25a19dfec219fa
Graeme <graeme.k.cox@gmail.com>
2018-03-05 22:01:14 -0500

    Added file reader functions under 'PreprocessingFunctions' folder

---
+++ PreprocessingFunctions/kaggleReader.py
@@ -0,0 +1,19 @@
+##kaggle reader
+import numpy as np 
+import scipy.io as spio
+
+fn = '/Users/graemecox/Documents/Capstone/Data/EEG_Data/Dog_1/Dog_1_ictal_segment_1.mat'
+
+mat = spio.loadmat(fn)
+
+print(mat['freq'])
+print(mat['latency'])
+print(np.array(mat['data']).shape)
+
+## Matlab structure fields:
+sampling_freq = mat['freq']
+latency = mat['latency']
+
+data = np.array(mat['data'])
+
+print(data.shape)
