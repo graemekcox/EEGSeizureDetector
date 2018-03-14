26bd1e274dc65b42ca24e9fd5dcddd2e0195880c
Graeme <graeme.k.cox@gmail.com>
2018-03-13 23:44:12 -0400

    Added function to test multiple classifiers

--- Classifier/linear_svm.py
+++ Classifier/linear_svm.py
@@ -5,12 +5,15 @@ sys.path.insert(0, '/Users/graemecox/Documents/Capstone/Code/eegSvm/Features')
 
 from fileReader import *
 
-from sklearn import datasets, svm, metrics
+from sklearn import datasets, svm, metrics, tree, neighbors, neural_network
+from sklearn import naive_bayes, linear_model
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import cross_val_score, train_test_split, ShuffleSplit
-from sklearn.neural_network import MLPClassifier 
-# from sklearn.tree import DecisionTreeClassifier
-from sklearn import tree
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.gaussian_process.kernels import RBF
+
 import cPickle
 import numpy as np 
 
@@ -21,14 +24,20 @@ CLASSIFIER_NAME = 'savedClassifier.pkl'
 
 CLASSIFIERS = [
 	svm.SVC(kernel='linear', C=0.035),
-	tree.DecisionTreeClassifier(max_depth=5)
+	tree.DecisionTreeClassifier(max_depth=5),
+	neighbors.KNeighborsClassifier(3),
+	neural_network.MLPClassifier(alpha=1),
+	naive_bayes.GaussianNB(),
+	svm.LinearSVC(),
+	LinearDiscriminantAnalysis(solver='svd', store_covariance=True),
+	QuadraticDiscriminantAnalysis(store_covariances=True),
+	# GaussianProcessClassifier(1.0*RBF(1.0)),
+	RandomForestClassifier(max_depth=5, n_estimators=10, max_features=1),
+	AdaBoostClassifier()
 	# tree.DecisionTreeClassifier(max_depth=5, n_estimators=10, max_features=1)
 	]
 # root = '/Users/graemecox/Documents/Capstone/Code/eegSvm/'
-CLASSIFIER_NAME = [
-	'SVM',
-	'Decision Tree Classifier'
-]
+
 
 
 
@@ -62,22 +71,25 @@ def train_multiple_classifiers(X_train, y_train, X_test, y_test, save=0):
 	for i in range(len(CLASSIFIERS)):
 		
 		temp_model = CLASSIFIERS[i]
-		print('%%%%%%%%%%%%Classifier %d: %s' % ((1+i),CLASSIFIER_NAME[i]))
+		print('%%%%%%%%%%%%  Classifier %d: %s' % ((1+i),CLASSIFIERS[i]))
 
 		temp_model.fit(X_train, y_train)
 		scores.append(temp_model.score(X_test,y_test))
 
 		print('Score: %f' % scores[i])
 
-	print(scores)
+	print('------------ FINISHED -----------')
+	return scores
 
 labels = np.load('../Data/labels.npy')
 feat = np.load('../Data/features.npy')
 X_train, X_test, y_train, y_test = train_test_split(
 feat, labels, test_size=0.2, random_state=0)
 
-train_multiple_classifiers(X_train, y_train, X_test, y_test,0)
-
+scores = train_multiple_classifiers(X_train, y_train, X_test, y_test,0)
+scores = np.array(scores)
+print('Classifier With the Highest Score: %s \nScore was: %s' % 
+	(CLASSIFIERS[scores.argmax()], scores[scores.argmax()]))
 
 	#RBF, AdaVB, SVM, 
 
