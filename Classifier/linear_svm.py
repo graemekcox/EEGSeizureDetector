5b4e202e0d4a6ba91c5f84f464888e5385f3ab2e
gramekcox <graeme.k.cox@gmail.com>
2018-03-19 22:37:20 -0400

    Added functions to optimize SVM parameters. These functinos seem to hang though, so not 100% it works yet.

--- Classifier/linear_svm.py
+++ Classifier/linear_svm.py
@@ -13,6 +13,11 @@ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticD
 from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.gaussian_process.kernels import RBF
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.metrics import roc_curve, auc
+from sklearn.preprocessing import label_binarize
 
 import cPickle
 import numpy as np 
@@ -43,8 +48,8 @@ CLASSIFIERS = [
 
 def createLinearClassifier(X_train, y_train,save=0):
 
+	# model = svm.SVC(C=25, kernel='rbf',degree=1)
 	model = svm.SVC(C=25, kernel='rbf',degree=1)
-
 	# Data stuff
 	y_train.ravel()
 
@@ -81,10 +86,106 @@ def train_multiple_classifiers(X_train, y_train, X_test, y_test, save=0):
 	print('------------ FINISHED -----------')
 	return scores
 
-# labels = np.load('../Data/labels.npy')
-# feat = np.load('../Data/features.npy')
-# X_train, X_test, y_train, y_test = train_test_split(
-# feat, labels, test_size=0.2, random_state=0)
+
+def optimizeSVM(X_train, y_train, X_test, y_test):
+	parameters= {'kernel':['linear','rbf'],
+	'C':[1,2,3,4,5,6,7,8,9,10],
+	'gamma':[0.01,0.02,0.03,0.04,0.05,0.10,0.2,0.3,0.4,0.5]}
+
+	# parameters = [
+ #  		{'C': [1, 10, 100, 1000], 'kernel': ['linear']},
+ #  		{'C': [1, 10, 100, 1000], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']},
+ # 	]
+
+	# parameters = {'kernel':('linear', 'rbf'),
+	#  'C':[1,2,3,4,5,6,7,8,9,10],
+	#  'gamma':[0.01,0.02,0.03,0.04,0.05,0.10,0.2,0.3,0.4,0.5]}
+
+	clf = svm.SVC()
+	grid = GridSearchCV(clf,parameters)
+	grid.fit(X_train, y_train)
+	predicted = grid.predict(X_test)
+	print('------ Confusion Matrix -----\n%s' % metrics.confusion_matrix(y_test,predicted))
+
+    # print('----- Confusion Matrix ------\n%s'%metrics.confusion_matrix(y_test, predicted ))
+
+def parameterScore(X_train, y_train, X_test, y_test):
+	
+	# parameters= {'kernel':['linear','rbf'],
+	# 'C':[1,2,3,4,5,6,7,8,9,10],
+	# 'gamma':[0.01,0.02,0.03,0.04,0.05,0.10,0.2,0.3,0.4,0.5]}
+
+	parameters= {'kernel':['linear','rbf'],
+	'C':[1,2,3,4,5,6,7,8,9,10],
+	'gamma':[0.01,0.05,0.10,0.20,0.30]}
+	scores = ['precision','recall']
+	# parameters= {'kernel':['linear','rbf'],
+	# 'C':[1,5,10],
+	# 'gamma':[0.01,0.5]}
+	# scores = ['precision','recall']
+
+
+
+	for score in scores:
+		print('##### Tuning hyper-parameters for %s\n' % score)
+
+		clf = GridSearchCV(svm.SVC(),  parameters, cv=5, scoring='%s_macro'%score)
+		clf.fit(X_train, y_train)
+
+		print("Best parameters set found on development set:\n")
+		print(clf.best_params_)
+		print("\nGrid scores on devlopment set:\n")
+		means = clf.cv_results_['mean_test_score']
+		stds = clf.cv_results_['std_test_score']
+		for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+			print("%0.3f (+/-%0.03f) for %r" % (mean, std*2, params))
+
+		print("\nClassification Report:\n")
+		y_true, y_pred = y_test, clf.predict(X_test)
+		print(classification_report(y_true,y_pred))
+
+
+
+
+
+labels = np.load('../Data/labels.npy')
+feat = np.load('../Data/features.npy')
+n_classes = 2
+
+# y = label_binarize(labels, classes=[-1, 1])
+# n_classes = y.shape[1]
+
+# random_state = np.random.RandomState(0)
+# n_samples, n_features = feat.shape
+# feat = np.c_[feat, random_state.randn(n_samples, 200 * n_features)]
+
+X_train, X_test, y_train, y_test = train_test_split(
+	feat, labels, test_size=0.2, random_state=0)
+
+# print('Starting classification')
+# # classifier = svm.SVC(C=5, kernel='linear',degree=1)
+# # y_score = classifier.fit(X_train, y_train)
+
+
+# classifier = OneVsRestClassifier(svm.SVC(kernel='linear', probability=True,
+#                                  random_state=random_state))
+# y_score = classifier.fit(X_train, y_train).decision_function(X_test)
+
+# print('Done Classification. Now plotting ROC curve')
+# # Compute ROC curve and ROC area for each class
+# fpr = dict()
+# tpr = dict()
+# roc_auc = dict()
+# for i in range(n_classes):
+#     fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+#     roc_auc[i] = auc(fpr[i], tpr[i])
+
+print(X_train.shape)
+
+
+print('Start optimize')
+# optimizeSVM(X_train, y_train, X_test, y_test)
+parameterScore(X_train, y_train, X_test, y_test)
 
 # scores = train_multiple_classifiers(X_train, y_train, X_test, y_test,0)
 # scores = np.array(scores)
